7e4671a79942351c529ff6c2e7368c75a82d735d
lonehusky76 <69285274+lonehusky76@users.noreply.github.com>
2026-01-29 23:25:04 -0500

    Delete Ca_analysis_matlab_JM directory

--- Ca_analysis_matlab_JM/AP_run_analysis_XLV.m
+++
@@ -1,7 +0,0 @@
-function AP_run_analysis(mainfile,parameterfile,folder,pk_threshold,paceability)
-fprintf(parameterfile);
-parameters = csvread(parameterfile); % call parameters
-[~, savename, ~] = fileparts(mainfile); % naming of output excel file
-AP_analysis_XLV(mainfile,savename,parameters(1,1),parameters(1,2),parameters(1,3),folder,pk_threshold,paceability);
-
-end
--- Ca_analysis_matlab_JM/getColumnName.m
+++
@@ -1,8 +0,0 @@
-function colName = getColumnName(colNumber)
-    colName = "";
-    while colNumber > 0
-        modulo = mod(colNumber - 1, 26);
-        colName = char('A' + modulo) + colName;
-        colNumber = floor((colNumber - modulo) / 26);
-    end
-end
